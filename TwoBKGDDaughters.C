19074b8cf78c98ca7dcb4f57f40a6a149ad12864
Zack Williams <zachary.williams2@mavs.uta.edu>
2020-05-07 04:09:23 -0500

    Other Opening Angles added

--- TwoBKGDDaughters.C
+++ TwoBKGDDaughters.C
@@ -366,9 +366,9 @@ void TwoBKGDDaughters::Loop()
       for (Int_t i = 0; i < nevents; i++) {
          t->GetEntry(i);
 
-	 //if (Event == event && Run == run && Subrun == subrun && (InteractionType != InteractionType_Check || (InteractionType == InteractionType_Check && CCNC != CCOrNC_Check)) && is_track) {
+	 if (Event == event && Run == run && Subrun == subrun && (InteractionType != InteractionType_Check || (InteractionType == InteractionType_Check && CCNC != CCOrNC_Check)) && is_track) {
 	 //if (Event == event && Run == run && Subrun == subrun && InteractionType == InteractionType_Check && CCNC == CCOrNC_Check && is_track) {
-	 if (Event == event && Run == run && Subrun == subrun && CCNC == CCOrNC_Check && is_track) {
+	 //if (Event == event && Run == run && Subrun == subrun && CCNC == CCOrNC_Check && is_track) {
             Matched = true;
 	    //if (jentry%10 == 0) std::cout<<"We Matched!"<<std::endl; // This is to see if we are actually making it to this point in the Matched condition!
 	    if (CC_Selected == 1) ccselected = true;
@@ -780,8 +780,8 @@ void TwoBKGDDaughters::Loop()
    //TFile *TDaughtersInfo = new TFile("CCQE_Daughter_Information.root", "RECREATE");
    //TFile *TDaughtersInfo = new TFile("CCRES_Daughter_Information.root", "RECREATE");
    //TFile *TDaughtersInfo = new TFile("CCDIS_Daughter_Information.root", "RECREATE");
-   //TFile *TDaughtersInfo = new TFile("Other_Daughter_Information.root", "RECREATE");
-   TFile *TDaughtersInfo = new TFile("CCInc_Daughter_Information.root", "CREATE");
+   TFile *TDaughtersInfo = new TFile("Other_Daughter_Information.root", "RECREATE");
+   //TFile *TDaughtersInfo = new TFile("CCInc_Daughter_Information.root", "RECREATE");
 
    hMuonMuonChi2->Write();
    hMuonProtonChi2->Write();
